53d3bd457aa3e994e3832c41e780c4901733df54
zabronax <zabronax@gmail.com>
2025-05-07 00:06:37 +0200

    setup development deployment

---
+++ infrastructure/environments/development/main.tf
@@ -0,0 +1,16 @@
+module "gitops_lite" {
+  source = "../../modules/gitops_lite"
+
+  server_type    = "cax21"
+  location       = "hel1"
+  ssh_public_key = file("id_rsa.pub")
+
+  git_source      = "https://github.com/LarsGJobloop/2025-soa-v2-composition"
+  target_branch   = "development"
+  compose_path    = "./compose.yaml"
+  update_interval = "*/1 * * * *"
+}
+
+output "ipv4_address" {
+  value = module.gitops_lite.server_ip
+}
