0f4e2a8f190855f28a2422327a0022eb12b94916
zabronax <zabronax@gmail.com>
2025-05-08 10:06:27 +0200

    add user specific endpoint

--- services/UserService/Program.cs
+++ services/UserService/Program.cs
@@ -2,5 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Welcome to the User Service");
+app.MapGet("/{UserId}", (int UserId) => $"Welcome user {UserId}");
 
 app.Run("http://0.0.0.0:80");
